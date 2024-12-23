72e6a473358923951077d4af0601473c91e22845
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2024-12-23 21:55:42 +0530

    CI/CD added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 // import authRoutes from './routes/auth';
 // import KAM from './models/KAM';
-
 
 dotenv.config();
 
@@ -19,11 +18,15 @@ app.use(bodyParser.json());
 // Routes
 // app.use('/api/auth', authRoutes);
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    console.log("req is here ............")
-    res.json({message: "got the req"})
-})
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log("req is here ............");
+  res.json({ message: "got the req" });
+});
 
+app.get("/ci", (req, res) => {
+  res.json({ message: "CI/ CD added" });
+});
 
-
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
